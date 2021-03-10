c9bee8d00c701de735219707ded1e467ffcbefef
Toby <40370496+DaatUserName@users.noreply.github.com>
2021-03-10 14:39:32 +0100

    idk

---
+++ src/database/Mongodb/models/userConfig.js
@@ -0,0 +1,18 @@
+
+/*
+const mongoose = require('mongoose');
+
+const UserConfig = new mongoose.model({
+    userName: { type: String, required: true },
+    userId: { type: String, required: true },
+     : { type: String, required: true },
+    userName: { type: String, required: true },
+    userName: { type: String, required: true },
+    userName: { type: String, required: true },
+    userName: { type: String, required: true },
+    userName: { type: String, required: true },
+    userName: { type: String, required: true },
+    userName: { type: String, required: true },
+})
+
+ */
